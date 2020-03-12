6f25afe055faa950785dd99948611a03d0f8ae51
Robert Gordon <Komeuppance@gmail.com>
2020-03-11 17:58:50 -0700

    Added app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,25 @@
 import React from 'react';
 import { render, fireEvent, waitForElement, wait } from '@testing-library/react'
 import App from './App';
-import { fetchMissions as mockFetchMissions }from './api/fetchMissions'
+import { fetchShow as mockFetchShow }from './api/fetchShow'
+
+jest.mock('./api/fetchShow');
+
+const shows = {
+    data: ['1', '2', '3']
+}
+
+test('app component fetches show and renders it', async () => {
+    // console.log('missiins', missions)
+    mockFetchShow.mockResolvedValueOnce(shows);
+  
+    const { getByText, queryAllByTestId } = render(<App/>);
+  
+    const button = getByText(/select a season/i);
+  
+    fireEvent.click(button);
+  
+    getByText(/season 1/i);
+    await wait();
+    expect(queryAllByTestId("show")).toHaveLength(0);
+  });
